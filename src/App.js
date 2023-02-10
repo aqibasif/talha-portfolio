806bd9ed42ed9f1c7fbbc84fb50e0830c7882442
Aqib Asif <80690135+aqibasif1@users.noreply.github.com>
2023-02-11 00:02:53 +0500

    minor update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>I'm Talha</div>;
+  return (
+    <div className='App'>
+      <span className='circle' />
+      <h1>I'm Talha Asif</h1>
+    </div>
+  );
 }
 
 export default App;
