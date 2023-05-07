a95f621658067cd3f084db363bda4562ce30a214
Aqib Asif <80690135+aqibasif1@users.noreply.github.com>
2023-05-07 21:45:52 +0500

    locomotive scroll test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import Home from "./pages/Home";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  return <Home />;
+  const containerRef = useRef(null);
+  // const [scrollEl, setScrollEl] = useState();
+
+  const LocomotiveOptions = {
+    smooth: true,
+    multiplier: 0.5,
+    lerp: 0.03,
+    smartphone: {
+      smooth: true,
+    },
+    tablet: {
+      smooth: true,
+    },
+  };
+
+  // useEffect(() => {
+  //   setScrollEl(containerRef.current);
+  // });
+
+  return (
+    <LocomotiveScrollProvider
+      watch={[]}
+      options={LocomotiveOptions}
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        {/* <ParallaxProvider scrollContainer={scrollEl}> */}
+        <Home />
+        {/* </ParallaxProvider> */}
+      </main>
+    </LocomotiveScrollProvider>
+  );
 }
 
 export default App;
